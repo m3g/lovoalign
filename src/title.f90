46c95b2fe989f2b271d3473a102acc3508e6c0dc
Leandro Martinez <leandromartinez98@gmail.com>
2016-08-05 11:27:12 -0300

    Version 16.218: Reorganized files to easiest maintainance

--- src/title.f90
+++ src/title.f90
@@ -10,7 +10,7 @@ subroutine title()
      &'                           PROGRAM: LOVOALIGN',/,&
      &'                   Low Order Value Optimization Methods',/,&
      &'                          for Protein Alignment',/,&
-     &'                              Version 16.217 ',/&
+     &'                              Version 16.218 ',/&
      &/,&
      &'  ',71('#'))")
 
