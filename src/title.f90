96e5ee84f7af1226b0d036dd262baa32a27bb6ba
Leandro Martinez <leandromartinez98@gmail.com>
2016-08-26 13:54:42 -0300

    Version 16.239: Added skip option and increased maxfiles default value

--- src/title.f90
+++ src/title.f90
@@ -10,7 +10,7 @@ subroutine title()
      &'                           PROGRAM: LOVOALIGN',/,&
      &'                   Low Order Value Optimization Methods',/,&
      &'                          for Protein Alignment',/,&
-     &'                              Version 16.222.3 ',/&
+     &'                              Version 16.239 ',/&
      &/,&
      &'  ',71('#'))")
 
