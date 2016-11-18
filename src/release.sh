384fd0b20dd467c2f6220e166c4e4fcb110a6711
Leandro Martinez <leandromartinez98@gmail.com>
2016-11-18 17:17:39 -0200

    link release zip file instead of .tar.gz

--- src/release.sh
+++ src/release.sh
@@ -23,17 +23,17 @@ year=`date +%y`
 day=`date +%j`
 version="${year:0:1}${year:1:1}.$day"
 
-file="$package-$version.tar.gz" 
+file="$package-$version.zip" 
 version_file=$version
 
 i=2
 while grep -q $file $downloads ; do
-  file=$package-$version.$i.tar.gz
+  file=$package-$version.$i.zip
   version_file=$version.$i
   i=`expr $i + 1`
 done
 version=$version_file
-file=$package-$version.tar.gz
+file=$package-$version.zip
 echo "Will create file: $file"
 
 cat $versionfile | sed -e "s/Version.*/Version\ $version \',\/\&/" > version_title_temp.f90
@@ -44,7 +44,7 @@ git commit -m "Changed version file to $version"
 git tag -a $version -m "Release $version"
 git push origin master tag $version
 
-newline="<tr><td width=190px valign=top><a href=$giturl/$version.tar.gz> $file </a></td><td> Release $version </td></tr>"
+newline="<tr><td width=190px valign=top><a href=$giturl/$version.zip> $file </a></td><td> Release $version </td></tr>"
 htmlfile=$downloads
 
 writeline=yes
