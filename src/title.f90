6877566df26400fe386a8e77713dc7b8c7c718d5
Leandro Martinez <leandromartinez98@gmail.com>
2017-06-23 13:00:15 -0300

    Changed version file to 17.174

--- src/title.f90
+++ src/title.f90
@@ -10,7 +10,7 @@ subroutine title()
      &'                           PROGRAM: LOVOALIGN',/,&
      &'                   Low Order Value Optimization Methods',/,&
      &'                          for Protein Alignment',/,&
-     &'                              Version 16.342 ',/&
+     &'                              Version 17.174 ',/&
      &/,&
      &'  ',71('#'))")
 
