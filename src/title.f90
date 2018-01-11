8fee11a9f6150df8561447ca8013c0027258ee80
Leandro Martinez <leandro@iqm.unicamp.br>
2018-01-11 17:02:48 -0200

    Changed version file to 18.011

--- src/title.f90
+++ src/title.f90
@@ -10,7 +10,7 @@ subroutine title()
      &'                                LOVOALIGN',/,&
      &'             Convergent algorithms for protein structural alignment',/,&
      &'     Martinez, Andreani, and Martinez, BMC Bioinformatics, 8:306, 2007',/,&
-     &'                              Version 17.175 ',/&
+     &'                              Version 18.011 ',/&
      &/,&
      &'  ',71('#'))")
 
